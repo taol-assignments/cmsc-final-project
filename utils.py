0b3eabb6bb61bc8269699c4aacb2fb2c6f2f07b9
Tao Liu <taol@mun.ca>
2020-06-21 11:58:38 -0230

    Move constants to utils.py

---
+++ utils.py
@@ -0,0 +1,35 @@
+PROVINCE_ABBR = {
+    "Alberta": "AB",
+    "British Columbia": "BC",
+    "Manitoba": "MB",
+    "New Brunswick": "NB",
+    "Newfoundland and Labrador": "NL",
+    "Nova Scotia": "NS",
+    "Northwest Territories": "NT",
+    "Nunavut": "NU",
+    "Ontario": "ON",
+    "Prince Edward Island": "PE",
+    "Quebec": "QC",
+    "Saskatchewan": "SK",
+    "Yukon": "YT"
+}
+
+
+COLORS = [
+    'blue',
+    'brown',
+    'orange',
+    'pink',
+    'green',
+    'gray',
+    'red',
+    'olive',
+    'purple',
+    'cyan',
+    'lime',
+    'deeppink',
+    'moccasin'
+]
+
+
+CSV_URL = "https://health-infobase.canada.ca/src/data/covidLive/covid19.csv"
