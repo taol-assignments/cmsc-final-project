bf795e72d7a32fca7d0f297ed7153a950e7f741c
Tao Liu <taol@mun.ca>
2020-06-23 00:04:05 -0230

    Fix prediction model.

--- predict_future_cases.py
+++ predict_future_cases.py
@@ -6,7 +6,13 @@ import matplotlib.pyplot as plt
 from province_wise_plots import init_date_xaxis
 
 
-def plot(dates: List[Timestamp], original: List[int], predict: List[int], actual: List[int]) -> None:
+def plot(
+        dates: List[Timestamp],
+        predict_len: int,
+        original: List[int],
+        compare: List[int],
+        actual: List[int],
+        predict: List[int]) -> None:
     """Plot the predict cases.
 
     Plot the predicted number of cases, actual numbers and previous numbers. And
@@ -14,6 +20,7 @@ def plot(dates: List[Timestamp], original: List[int], predict: List[int], actual
 
     Arguments:
         dates: The date list.
+        predict_len: The predict time length.
         original: The original data list before the prediction date.
         predict: Predicted data list.
         actual: Actual data list.
@@ -24,14 +31,15 @@ def plot(dates: List[Timestamp], original: List[int], predict: List[int], actual
 
     init_date_xaxis()
     plt.plot(dates[:len(original)], original, label='Original', color='blue')
-    plt.plot(dates[len(original):], predict, label='Predict', color='red')
-    plt.plot(dates[len(original):], actual, label='Actual',  color='green')
+    plt.plot(dates[len(original):len(original) + predict_len], compare, label='Predict', color='yellow')
+    plt.plot(dates[len(original):len(original) + predict_len], actual, label='Actual',  color='red')
+    plt.plot(dates[len(original) + predict_len:], predict, label='Future Prediction',  color='green')
     plt.legend()
 
     # Eliminate the margin on the x axis.
     plt.margins(x=0)
 
-    plt.title('Predicted Cases')
+    plt.title('Predicted New Cases per Day')
 
     # Set bbox_inches to 'tight' to remove padding.
     plt.savefig('future_cases.png', bbox_inches='tight')
@@ -41,11 +49,36 @@ def main():
     """The main function."""
 
     data, dates, provinces = get_csv()
-    num_total = data[data['prname'] == 'Canada']['numtotal'].tolist()
 
-    plen = 20
-    model = auto_arima(num_total[:-plen])
-    plot(dates, num_total[:-plen], list(map(int, model.predict(plen))), num_total[-plen:])
+    plen = 14
+
+    # Extend the date list.
+    for i in range(plen):
+        dates.append(dates[-1] + pd.Timedelta(days=1))
+
+    num_total = data[data['prname'] == 'Canada']['numtoday'].tolist()
+
+    model = auto_arima(
+        num_total[:-plen],
+        trend='ct',
+        seasonal=False)
+    compare = list(map(int, model.predict(plen)))
+
+    model = auto_arima(
+        num_total,
+        P=15,
+        trend='ct',
+        seasonal=False)
+
+    predict = list(map(int, model.predict(plen)))
+
+    plot(
+        dates,
+        plen,
+        num_total[:-plen],
+        compare,
+        num_total[-plen:],
+        predict)
 
 
 if __name__ == '__main__':
